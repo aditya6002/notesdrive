41a0f40df3a3101753799667978fda471b18b4dc
aditya6002 <adityaofficial690@gmail.com>
2026-08-14 21:34:51 +0530

    note routes added

--- backend/src/routes/notes.routes.js
+++ backend/src/routes/notes.routes.js
@@ -1,6 +1,30 @@
 const express = require("express");
 const router = express.Router();
 
-// router.post("/");
+router.post("/", (req, res) => {
+  res.status(201).json({ msg: "new note created", data: req.body });
+});
+
+router
+  .route("/:noteId")
+  .get((req, res) => {
+    res.status(200).json({ msg: "note found", data: "note" });
+  })
+
+  .put((req, res) => {
+    res.status(200).json({ msg: "note updated", data: "deleted note" });
+  })
+
+  .delete((req, res) => {
+    res.status(200).json({ msg: "note deleted" });
+  });
+
+router.get("/shared/:noteId", (req, res) => {
+  res.status(200).json({ msg: "shared note found", data: "shared note" });
+});
+
+router.post("/report/:noteId", (req, res) => {
+  res.status(200).json({ msg: "note reported", data: "reported note" });
+});
 
 module.exports = router;
