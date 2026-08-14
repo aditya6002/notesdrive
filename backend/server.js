80e05f5446dd06b9170e0bd172e85a24a430dc5b
aditya6002 <adityaofficial690@gmail.com>
2026-08-14 21:06:48 +0530

    initialize backend server

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+
+const app = require("./src/app");
+const connectDB = require("./src/db/db");
+
+// Connect to database
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+}); 
+
+
