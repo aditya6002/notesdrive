7b4a960d0032bb905312130a36abcab32c2dc739
aditya6002 <adityaofficial690@gmail.com>
2026-08-16 13:27:01 +0530

    fix nodemailer

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -1,46 +1,58 @@
-require('dotenv').config();
+require("dotenv").config();
 const nodeMailer = require("nodemailer");
 
+// const transporter = nodeMailer.createTransport({
+//     service : 'gmail',
+//     auth :{
+//         type: 'OAuth2',
+//         user: process.env.EMAIL_USER,
+//         clientId: process.env.CLIENT_ID,
+//         clientSecret: process.env.CLIENT_SECRET,
+//         refreshToken: process.env.REFRESH_TOKEN,
+//     }
+// });
+
+/**
+ * Working nodemailer
+ */
 const transporter = nodeMailer.createTransport({
-    service : 'gmail',
-    auth :{
-        type: 'OAuth2',
-        user: process.env.EMAIL_USER,
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        refreshToken: process.env.REFRESH_TOKEN,
-    }
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: process.env.EMAIL_SECURE === "true",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
-transporter.verify((error,success)=>{
-    if(error){
-        console.error("Error in email transporter: ", error);
-    }else{
-        console.log("Email transporter is ready to send emails!");
-    }
-})
+transporter.verify((error, success) => {
+  if (error) {
+    console.error("Error in email transporter: ", error);
+  } else {
+    console.log("Email transporter is ready to send emails!");
+  }
+});
 
 /**
  * Generic email sender
  */
 
-const sendEmail = async(to , subject ,text, html)=>{
-    try{
-        const info = await transporter.sendMail({
-            from : `"BACKEND LEDGER" <${process.env.EMAIL_USER}>`,
-            to,
-            subject,
-            text,
-            html
-        });
-        console.log("Email sent: ",info.MessageId);
-    }catch(err){
-        console.error("Error in sending email :",err);
-    }
-}
+const sendEmail = async (to, subject, text, html) => {
+  try {
+    const info = await transporter.sendMail({
+      from: `"BACKEND LEDGER" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      text,
+      html,
+    });
+    console.log("Email sent: ", info.MessageId);
+  } catch (err) {
+    console.error("Error in sending email :", err);
+  }
+};
 
 async function sendRegistrationEmail(userEmail, name) {
-
   const subject = "Welcome to NoteDrive! 🎉";
 
   const text = `Hello ${name},
@@ -85,5 +97,5 @@ NoteDrive Team
 
 module.exports = {
   sendEmail,
-  sendRegistrationEmail
+  sendRegistrationEmail,
 };
